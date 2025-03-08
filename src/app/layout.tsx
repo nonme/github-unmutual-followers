a97931a15e25971e7585c54cd63bf0c46a375b6c
nonme <xdock1good@gmail.com>
2025-03-08 13:41:24 +0200

    Add Google Site Verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="Vcev7wasBlc4CWcZOjEJETOtVyAShbLPbbYsNU02_2o"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
